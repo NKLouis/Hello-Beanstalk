1f578380ca45ef87a2bf7abe7768db07aa1c3d38
naritchaya louis <naritchayalouis@gmail.com>
2021-06-17 23:14:44 -0500

    added controller folder

---
+++ src/main/java/com/trilogyed/hellobeanstalk/HelloBeanstalkApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.hellobeanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBeanstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBeanstalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilogyed/hellobeanstalk/HelloBeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.hellobeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
